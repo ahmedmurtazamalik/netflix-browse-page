cbf3974cb72425a75935b594cc84ed6f9dc0b335
Ahmed Murtaza Malik <114727657+ahmedmurtazamalik@users.noreply.github.com>
2023-07-12 01:37:56 +0500

    scrollable thumbnails

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 window.addEventListener('resize', function() {
     var nav1list = document.getElementById('navbar1list');
+    var browse = document.getElementById('browse');
     var windowWidth = window.innerWidth || document.documentElement.clientWidth;
   
     if (windowWidth <= 768) {
       nav1list.style.display = 'none';
+      browse.style.display = 'flex';
     } else {
       nav1list.style.display = 'flex';
+      browse.style.display = 'none';
     }
   });
+
+
+  const slides = document.querySelectorAll(".slide")
+  console.log(slides)
+  var counter = 0;
+
+  slides.forEach(
+    (slide,index) => {
+      slide.style.left = `${index * 100}% `
+    }
+  )
