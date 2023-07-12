bfd5dedc0c5f8b4a39fbacd7e3213d8dc8c33d4b
Ahmed Murtaza Malik <114727657+ahmedmurtazamalik@users.noreply.github.com>
2023-07-13 01:10:50 +0500

    foot

--- index.js
+++ index.js
@@ -11,6 +11,50 @@
       browse.style.display = 'none';
     }
   }
-  
   window.addEventListener('resize', headerSizeChanges);
   window.addEventListener('DOMContentLoaded', headerSizeChanges);
+ 
+  let wonkaImage = document.createElement('img');
+  wonkaImage.classList.add("sectionimage");
+  wonkaImage.src = 'data/wonka-thumbnail.jpg';
+  wonkaImage.alt = 'wonka-thumbnail';
+
+  let oppenheimerImage = document.createElement('img');
+  oppenheimerImage.classList.add("sectionimage");
+  oppenheimerImage.src = 'data/oppenheimer-thumbnail.jpg';
+  oppenheimerImage.alt = 'oppenheimer-thumbnail';
+
+  let napoleonImage = document.createElement('img');
+  napoleonImage.classList.add("sectionimage");
+  napoleonImage.src = 'data/napoleon-thumbnail.jpg';
+  napoleonImage.alt = 'napoleon-thumbnail';
+
+  let miImage = document.createElement('img');
+  miImage.classList.add("sectionimage");
+  miImage.src = 'data/mi-thumbnail.jpg';
+  miImage.alt = 'mi-thumbnail';
+
+  let kotfmImage = document.createElement('img');
+  kotfmImage.classList.add("sectionimage");
+  kotfmImage.src = 'data/kotfm-thumbnail.jpg';
+  kotfmImage.alt = 'kotfm-thumbnail';
+
+
+  const thumbnails = [];
+  thumbnails.push(wonkaImage);
+  thumbnails.push(oppenheimerImage);
+  thumbnails.push(napoleonImage);
+  thumbnails.push(miImage);
+  thumbnails.push(kotfmImage);
+
+  const imageSections = document.getElementsByClassName('section');
+
+  for (let i = 0; i < imageSections.length; i++) {
+    const singlesection = imageSections[i];
+    for (let j = 0; j < 15; j++) {
+      const randomIndex = Math.floor(Math.random() * thumbnails.length);
+      console.log(randomIndex);
+      const randomThumbnail = thumbnails[randomIndex].cloneNode(true);
+      singlesection.appendChild(randomThumbnail);
+    }
+  }
